5b1fc5f54f99a8e3c0b77d7d12d282bf60845169
Humphries <humphries@cs.unc.edu>
2021-02-19 20:49:54 -0500

    Typo fix to addMuslFilePath.sh

--- TASEConfig/addMuslFilePath.sh
+++ TASEConfig/addMuslFilePath.sh
@@ -1,4 +1,4 @@
 set -e
 
-echo "MUSL_PATH=$(pwd)/.." >> config.mak_no_simd
-echo "MUSL_PATH=$(pwd)/.." >> config.mak_simd
+echo "MUSL_DIR=$(pwd)/.." >> config.mak_no_simd
+echo "MUSL_DIR=$(pwd)/.." >> config.mak_simd
